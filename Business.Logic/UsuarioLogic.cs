a5cf0ac3685900638b7823cc65e9b73fb9cbc676
Santiago Garcia <santiagogarcia97@outlook.com>
2018-05-24 22:27:07 -0300

    Laboratorio 3 (falta terminar la ultima parte y algunas cosas de la segunda)

---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Database;
+using Business.Entities;
+
+namespace Business.Logic {
+    public class UsuarioLogic : BusinessLogic {
+
+        private UsuarioAdapter _UsuarioData;
+        
+        public UsuarioAdapter UsuarioData {
+            get { return _UsuarioData; }
+            set { _UsuarioData = value; }
+        }
+
+        public UsuarioLogic() {
+
+
+        }
+
+        public List<Usuario> GetAll() {
+            return UsuarioData.GetAll();
+
+        }
+
+        public Usuario GetOne(int id) {
+            return UsuarioData.GetOne(id);
+        }
+
+        public void Delete(int id) {
+            UsuarioData.Delete(id);
+        }
+
+        public void Save(Usuario user) {
+            UsuarioData.Save(user);
+        }
+
+
+
+    }
+}
