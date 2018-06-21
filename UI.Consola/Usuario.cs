1b0abad946a9bcc8a9567c737e5df3d42f0a6737
Joaquin Cardona Ruiz <joaquincardonaruiz@gmail.com>
2018-06-21 03:28:29 +0100

    Lab2.3 Terminado

--- UI.Consola/Usuario.cs
+++ UI.Consola/Usuario.cs
@@ -17,35 +17,42 @@ namespace UI.Consola {
             UsuarioNegocio = new UsuarioLogic();
         }
         public void Menu() {
+            while (true) {
+                //Mostrar listado de menu principal 4.6
+                Console.Clear();
+                Console.WriteLine("Menu principal\n1- Listado General\n2- Consulta\n3- Agregar\n4- Modificar\n5- Eliminar\n6- Salir");
 
-            //Mostrar listado de menu principal 4.6
-            Console.WriteLine("Menu principal\n1- Listado General\n2- Consulta\n3- Agregar\n4- Modificar\n5- Eliminar\n6- Salir");
-
-            ConsoleKeyInfo op = Console.ReadKey();
-            switch (op.Key) {
-                case ConsoleKey.D1: {
-                        this.ListadoGeneral();
-                        break;
-                    }
-                case ConsoleKey.D2: {
-                        this.Consultar();
-                        break;
-                    }
-                case ConsoleKey.D3: {
-                        this.Agregar();
-                        break;
-                    }
-                case ConsoleKey.D4: {
-                        this.Modificar();
-                        break;
-                    }
-                case ConsoleKey.D5: {
-                        this.Eliminar();
-                        break;
-                    }
+                ConsoleKeyInfo op = Console.ReadKey();
+                switch (op.Key) {
+                    case ConsoleKey.D1: {
+                            this.ListadoGeneral();
+                            break;
+                        }
+                    case ConsoleKey.D2: {
+                            this.Consultar();
+                            break;
+                        }
+                    case ConsoleKey.D3: {
+                            this.Agregar();
+                            break;
+                        }
+                    case ConsoleKey.D4: {
+                            this.Modificar();
+                            break;
+                        }
+                    case ConsoleKey.D5: {
+                            this.Eliminar();
+                            break;
+                        }
+                    case ConsoleKey.D6: {
+                            System.Environment.Exit(1);
+                            break;
+                        }  
+                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
         }
+
         public void ListadoGeneral() {
             Console.Clear();
             foreach (Business.Entities.Usuario usr in UsuarioNegocio.GetAll()) {
@@ -71,17 +78,79 @@ namespace UI.Consola {
             finally {
                 Console.WriteLine();
                 Console.WriteLine("Presione una tecla para continuar");
-                Console.ReadKey();
             }
         }
         public void Agregar() {
-
+            Console.Clear();
+            Business.Entities.Usuario usuario = new Business.Entities.Usuario();
+            Console.Write("Ingrese Nombre: ");
+            usuario.Nombre = Console.ReadLine();
+            Console.Write("Ingrese Apellido: ");
+            usuario.Apellido = Console.ReadLine();
+            Console.Write("Ingrese Nombre de Usuario: ");
+            usuario.NombreUsuario = Console.ReadLine();
+            Console.Write("Ingrese Clave: ");
+            usuario.Clave = Console.ReadLine();
+            Console.Write("Ingrese Email: ");
+            usuario.Email = Console.ReadLine();
+            Console.Write("Ingrese Habilitación de usuario (1-Si/otro-No): ");
+            usuario.Habilitado = (Console.ReadLine() == "1");
+            usuario.State = BusinessEntity.States.New;
+            UsuarioNegocio.Save(usuario);
+            Console.WriteLine();
+            Console.WriteLine("ID: {0}", usuario.ID);
         }
         public void Modificar() {
-
+            try { 
+                Console.Clear();
+                Console.Write("Ingrese el ID del usuario a modificar: ");
+                int id = int.Parse(Console.ReadLine());
+                Business.Entities.Usuario usuario = UsuarioNegocio.GetOne(id);
+                Console.Write("Ingrese Nombre: ");
+                usuario.Nombre = Console.ReadLine();
+                Console.Write("Ingrese Apellido: ");
+                usuario.Apellido = Console.ReadLine();
+                Console.Write("Ingrese Nombre de Usuario: ");
+                usuario.NombreUsuario = Console.ReadLine();
+                Console.Write("Ingrese Clave: ");
+                usuario.Clave = Console.ReadLine();
+                Console.Write("Ingrese Email: ");
+                usuario.Email = Console.ReadLine();
+                Console.Write("Ingrese Habilitación de usuario (1-Si/otro-No): ");
+                usuario.Habilitado = (Console.ReadLine() == "1");
+                usuario.State = BusinessEntity.States.Modified;
+                UsuarioNegocio.Save(usuario);
+            } 
+            catch(FormatException fe) {
+                Console.WriteLine();
+                Console.WriteLine("La ID ingresada debe ser un número entero");
+            } 
+            catch(Exception e) {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            } 
+            finally {
+                Console.WriteLine("Presione una tecla para continuar");
+            }
         }
         public void Eliminar() {
-
+            try {
+                Console.Clear();
+                Console.Write("Ingrese el ID del usuario a eliminar: ");
+                int id = int.Parse(Console.ReadLine());
+                UsuarioNegocio.Delete(id);
+            }
+            catch(FormatException fe){
+                Console.WriteLine();
+                Console.WriteLine("La ID ingresada debe ser un numero entero");
+            }
+            catch(Exception e) {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("Presione una tecla para continuar");
+            }
         }
         public void MostrarDatos(Business.Entities.Usuario usr) {
             //Mostrar datos, paso 4.8
